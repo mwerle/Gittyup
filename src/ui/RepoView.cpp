b18f13a787e0640902492b8b324fb2cc9482acee
Michael Werle <micha@michaelwerle.com>
2023-08-29 14:16:28 +0900

    chore: fix more warnings.
    Temporarily enabled `-Wall` and fixed more warnings, but there are several uninitialized variables usages which may require review by the code author in order to ensure the refactor is correct so we leave these additional warnings disabled for now.
    
    Fixed:
    * removed unused lambda captures
    * removed unused variables
    * fixed sign comparisons between variables

--- src/ui/RepoView.cpp
+++ src/ui/RepoView.cpp
@@ -1457,7 +1457,7 @@ void RepoView::rebaseAboutToRebase(const git::Rebase rebase,
   QString beforeText = before.link();
   QString step = tr("%1/%2").arg(currIndex).arg(rebase.count());
   QString text = tr("%1 - %2").arg(step, beforeText);
-  LogEntry *entry = mRebase->addEntry(text, tr("Apply"));
+  mRebase->addEntry(text, tr("Apply"));
 }
 
 void RepoView::rebaseConflict(const git::Rebase rebase) {
@@ -2780,14 +2780,15 @@ void RepoView::refresh() { refresh(true); }
 
 void RepoView::refresh(bool restoreSelection) {
   // Fake head update.
-  uint32_t counter = 0;
   auto dtw = findChild<DoubleTreeWidget *>();
-  if (dtw)
-    counter = dtw->setDiffCounter();
-  if (mRepo.head().isValid())
+  if (dtw) {
+    dtw->setDiffCounter();
+  }
+  if (mRepo.head().isValid()) {
     DebugRefresh("Head name: " << mRepo.head().name());
-  else
+  } else {
     DebugRefresh("Head invalid");
+  }
   DebugRefresh("time: " << QDateTime::currentDateTime()
                         << " Set diff counter: " << counter);
   emit mRepo.notifier()->referenceUpdated(mRepo.head(), restoreSelection);
